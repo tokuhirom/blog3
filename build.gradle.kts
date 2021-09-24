37cf53108fc22d78b7f9975ab30bfcb31ac70a35
Tokuhiro Matsuno <tokuhirom@gmail.com>
2021-09-24 09:11:39 +0900

    enable actuator

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("org.springframework.boot") version "2.5.4"
