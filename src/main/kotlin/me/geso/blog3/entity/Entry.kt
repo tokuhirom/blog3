041746ca37b6232a271c13df371ad295b2abff53
Tokuhiro Matsuno <tokuhirom@gmail.com>
2021-09-28 10:44:34 +0900

    enable autolink plugin for flexmark

--- src/main/kotlin/me/geso/blog3/entity/Entry.kt
+++ src/main/kotlin/me/geso/blog3/entity/Entry.kt
@@ -1,14 +1,32 @@
 package me.geso.blog3.entity
 
+import com.vladsch.flexmark.ext.autolink.AutolinkExtension
 import com.vladsch.flexmark.html.HtmlRenderer
 import com.vladsch.flexmark.parser.Parser
 import com.vladsch.flexmark.util.data.MutableDataSet
-import org.jetbrains.annotations.NotNull
 import java.time.LocalDateTime
 
-var options: MutableDataSet = MutableDataSet()
-var parser: Parser = Parser.builder(options).build()
-var renderer: HtmlRenderer = HtmlRenderer.builder(options).build()
+
+class MarkdownRenderer(val parser: Parser, val renderer: HtmlRenderer) {
+
+    fun render(mkdn: String): String {
+        val document = parser.parse(mkdn)
+        return renderer.render(document)
+    }
+
+    companion object {
+        @JvmStatic
+        fun build(): MarkdownRenderer {
+            val options = MutableDataSet()
+            options.set(Parser.EXTENSIONS, listOf(AutolinkExtension.create()))
+            val parser = Parser.builder(options).build()
+            val renderer = HtmlRenderer.builder(options).build()
+            return MarkdownRenderer(parser, renderer)
+        }
+    }
+}
+
+val renderer = MarkdownRenderer.build()
 
 data class Entry(
     val path: String,
@@ -21,8 +39,7 @@ data class Entry(
 ) {
     fun html(): String {
         return if (format == "mkdn") {
-            val document = parser.parse(body)
-            renderer.render(document)
+            renderer.render(body)
         } else {
             body
         }
