586adc573497866a9bb5dc63e633b60808852030
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-06-04 12:36:49 +0900

    Update bootstrap to 5.3.3 (#621)

--- src/main/kotlin/blog3/web/view/Wrapper.kt
+++ src/main/kotlin/blog3/web/view/Wrapper.kt
@@ -39,7 +39,7 @@ suspend fun PipelineContext<Unit, ApplicationCall>.publicWrapper(
             meta(charset = "utf-8")
             title(title)
             link(
-                href = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+                href = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
                 rel = "stylesheet",
             ) {
                 integrity = "sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
