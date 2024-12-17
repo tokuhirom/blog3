04ead25ba7ebe431bf55fce54db47e52bcde7670
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-12-17 16:03:25 +0900

    fix path

--- src/main/kotlin/blog3/admin/service/S3Service.kt
+++ src/main/kotlin/blog3/admin/service/S3Service.kt
@@ -23,13 +23,12 @@ data class S3ConfigurationProperties(
 @EnableConfigurationProperties(S3ConfigurationProperties::class)
 class S3Configuration {
     @Bean
-    fun s3Client(s3ConfigurationProperties: S3ConfigurationProperties): S3Service {
-        return S3Service(
+    fun s3Client(s3ConfigurationProperties: S3ConfigurationProperties): S3Service =
+        S3Service(
             region = s3ConfigurationProperties.region,
             bucketName = s3ConfigurationProperties.bucketName,
             publicDomain = s3ConfigurationProperties.publicDomain,
         )
-    }
 }
 
 class S3Service(
@@ -41,32 +40,39 @@ class S3Service(
     private val logger = KotlinLogging.logger {}
 
     // Build S3 client using ENV[AWS_ACCESS_KEY_ID], ENV[AWS_SECRET_KEY]
-    private val s3Client = AmazonS3ClientBuilder.standard()
-        .withCredentials(credentialProvider)
-        .withRegion(region)
-        .build()
+    private val s3Client =
+        AmazonS3ClientBuilder
+            .standard()
+            .withCredentials(credentialProvider)
+            .withRegion(region)
+            .build()
 
-    fun listBuckets(): List<Bucket> {
-        return s3Client.listBuckets()!!
-    }
+    fun listBuckets(): List<Bucket> = s3Client.listBuckets()!!
 
-    fun upload(key: String, content: String): String {
+    fun upload(
+        key: String,
+        content: String,
+    ): String {
         s3Client.putObject(bucketName, key, content)!!
-        return "https://${publicDomain}/${key}"
+        return "https://$publicDomain/$key"
     }
 
-    fun upload(key: String, inputStream: InputStream, metadata: ObjectMetadata): String {
+    fun upload(
+        key: String,
+        inputStream: InputStream,
+        metadata: ObjectMetadata,
+    ): String {
         logger.info("Uploading file: bucketName=$bucketName key=$key region=${s3Client.regionName}")
         s3Client.putObject(bucketName, key, inputStream, metadata)!!
-        return "https://${publicDomain}/${key}"
+        return "https://$publicDomain/$key"
     }
 }
 
-//fun main() {
+// fun main() {
 //    val client = S3Service(
 //        region = "ap-northeast-1", bucketName = "blog3-attachments",
 //        publicDomain = "blog-attachments.64p.org"
 //    )
 //    val key = "hello.txt"
 //    println(client.upload(key, "haha"))
-//}
+// }
