98c7bbd938d6431fa5bc1c1c52941be394efbe3d
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-12-20 10:28:57 +0900

    remove shell utils

--- blog-admin/devtools/post.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-curl -X POST http://localhost:5173/api/entry \
--H "Content-Type: application/json" \
--d '{
-  "title": "Test Entry",
-  "body": "This is a test entry body.",
-  "status": "draft"
-}'
