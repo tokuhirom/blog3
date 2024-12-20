30f4d4fc0d5f0fb035fb27c28c62da6cdfcf5395
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-12-20 12:11:49 +0900

    ptimize imports

--- blog-admin/src/routes/entry/[path]/+page.server.ts
+++ blog-admin/src/routes/entry/[path]/+page.server.ts
@@ -1,13 +1,12 @@
-import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-import { db, EntryModel } from '$lib/db';
+import { EntryModel } from '$lib/db';
 
 export const load: PageServerLoad = async ({ params }) => {
 	console.log('Loading entry page content!');
 
 	const path = params.path;
 
-    const entry = await EntryModel.getEntry(path);
+	const entry = await EntryModel.getEntry(path);
 	console.log('Entry loaded: ', entry);
 	return {
 		entry: entry
