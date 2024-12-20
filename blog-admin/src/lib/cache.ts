faf2187578e54d532b439c80db13f382ee749159
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-12-20 13:15:22 +0900

    use custom css

--- blog-admin/src/lib/cache.ts
+++ blog-admin/src/lib/cache.ts
@@ -1,21 +1,21 @@
-import { EntryModel, type Entry } from "./db";
+import { EntryModel, type Entry } from './db';
 
 let cache: Entry[] | null = null;
 
 export class EntryCache {
-    static async get() : Promise<Entry[]> {
-        if (cache == null) {
-            console.log("Cache miss");
-            const got = await EntryModel.getAllEntries();
-            cache = got;
-            return got;
-        } else {
-            console.log("Cache hit");
-            return cache;
-        }
-    }
+	static async get(): Promise<Entry[]> {
+		if (cache == null) {
+			console.log('Cache miss');
+			const got = await EntryModel.getAllEntries();
+			cache = got;
+			return got;
+		} else {
+			console.log('Cache hit');
+			return cache;
+		}
+	}
 
-    static clear() {
-        cache = null;
-    }
+	static clear() {
+		cache = null;
+	}
 }
--- blog-admin/src/routes/EntryList.svelte
+++ blog-admin/src/routes/EntryList.svelte
@@ -4,14 +4,24 @@
 	export let entries: Entry[] = [];
 </script>
 
-<div class="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
+<div class="list">
 	{#each entries as entry}
-		<a
-			class="w-full max-w-[200px] break-all rounded bg-white p-4 shadow-md"
-			href={`/entry/${encodeURIComponent(entry.path)}`}
-		>
-			<h2 class="mb-2 font-bold">{entry.title}</h2>
-			<p class="text-sm text-gray-600">{entry.body.slice(0, 100)}...</p>
+		<a class="item break-all shadow-md" href={`/entry/${encodeURIComponent(entry.path)}`}>
+			<h2 class="mb-2 text-sm font-bold">{entry.title}</h2>
+			<p class="text-xs text-gray-600">{entry.body.slice(0, 100)}...</p>
 		</a>
 	{/each}
 </div>
+
+<style>
+	.list {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+		gap: 1rem;
+	}
+
+	.item {
+		width: 100%;
+		padding: 1rem;
+	}
+</style>
