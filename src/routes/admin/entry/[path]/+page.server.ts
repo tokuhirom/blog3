938ab6c3cb556b50cd39c40a798ac714f8c726c3
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-12-23 16:52:50 +0900

    fix: add Actions type annotation in +page.server.ts

--- src/routes/admin/entry/[path]/+page.server.ts
+++ src/routes/admin/entry/[path]/+page.server.ts
@@ -1,4 +1,4 @@
-import type { PageServerLoad } from './$types';
+import type { PageServerLoad, Actions } from './$types';
 import { redirect } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({ params, locals }) => {
