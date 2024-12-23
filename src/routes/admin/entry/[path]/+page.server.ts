173535942cc0a31b3a6544ecbf6547a8be823692
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-12-24 00:23:19 +0900

    Implement CSRF protection and refactor entry update handling to use JSON requests

--- src/routes/admin/entry/[path]/+page.server.ts
+++ src/routes/admin/entry/[path]/+page.server.ts
@@ -11,24 +11,6 @@ export const load: PageServerLoad = async ({ params, locals }) => {
 };
 
 export const actions: Actions = {
-	update: async ({ request, params, locals }) => {
-		const formData = await request.formData();
-		const title = formData.get('title') as string;
-		const body = formData.get('body') as string;
-		const status = formData.get('status') as 'draft' | 'published';
-		const path = params.path;
-
-		// 必須フィールドチェック
-		if (!title || !body || !status) {
-			return { error: 'Missing required fields' };
-		}
-
-		console.log(
-			`Updating entry with path: ${path}, title: ${title}, body: ${body}, status: ${status}`
-		);
-		await locals.adminEntryRepository.updateEntry(path, { title, body, status });
-		return { success: true };
-	},
 	delete: async ({ params, locals }) => {
 		const path = params.path;
 
--- src/routes/admin/entry/[path]/+page.svelte
+++ src/routes/admin/entry/[path]/+page.svelte
@@ -4,6 +4,8 @@
 	import type { PageData } from './$types';
 
 	import MarkdownEditor from '$lib/components/admin/MarkdownEditor.svelte';
+	import { updateTask } from '@vitest/runner';
+	import { preventDefault } from 'svelte/legacy';
 
 	let { data }: { data: PageData } = $props();
 	if (!data.entry) {
@@ -34,6 +36,34 @@
 			}
 		}
 	}
+
+	async function handleUpdate() {
+		successMessage = '';
+		errorMessage = '';
+
+		try {
+			const request = {
+				title,
+				body,
+				status,
+			}
+			const response = await fetch("/admin/api/entry/" + entry.path, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(request),
+			})
+			if (response.ok) {
+				successMessage = 'Entry updated successfully';
+			} else {
+				errorMessage = 'Failed to update entry';
+			}
+		} catch (e) {
+			errorMessage = 'Failed to update entry';
+			console.error('Failed to update entry:', e);
+		}
+	}
 </script>
 
 <form
@@ -41,9 +71,9 @@
 	method="post"
 	action="?/update"
 	class="space-y-4 p-4"
-	onsubmit={() => {
-		successMessage = '';
-		errorMessage = '';
+	onsubmit={async (event) => {
+		event.preventDefault();
+		await handleUpdate();
 	}}
 >
 	<div>
@@ -69,7 +99,7 @@
 			onSave={(content) => {
 				body = content;
 				console.log('Save the content by shortcut');
-				form.submit();
+				handleUpdate();
 			}}
 		></MarkdownEditor>
 	</div>
