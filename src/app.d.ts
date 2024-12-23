114a8ae1968ff0c79029687c4d8cd3797fdd6610
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-12-23 16:31:03 +0900

    fix: add adminEntryRepository to Locals interface in app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,14 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+
+import { AdminEntryRepository } from '$lib/db';
+
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			adminEntryRepository: AdminEntryRepository;
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
