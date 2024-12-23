6b44fcf5fb8e12e1bd9a47cf97592f50548097fe
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-12-24 00:28:27 +0900

    Update fetch URL in MarkdownEditor to use the correct admin API endpoint (#755)

--- src/lib/components/admin/MarkdownEditor.svelte
+++ src/lib/components/admin/MarkdownEditor.svelte
@@ -96,7 +96,7 @@
 		const formData = new FormData();
 		formData.append('file', file);
 
-		const response = await fetch('/api/upload', {
+		const response = await fetch('/admin/api/upload', {
 			method: 'POST',
 			body: formData
 		});
