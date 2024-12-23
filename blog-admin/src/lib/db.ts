ee58b601bfc56ae3030d8b53afe494f6ba4ce0cc
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-12-23 15:43:25 +0900

    check the required environment variables before run

--- blog-admin/src/lib/db.ts
+++ blog-admin/src/lib/db.ts
@@ -4,6 +4,23 @@ import { format } from 'date-fns';
 
 dotenv.config();
 
+// Check the environment variables
+const requiredEnvVars = [
+	'DATABASE_HOST',
+	'DATABASE_PORT',
+	'DATABASE_USER',
+	'DATABASE_PASSWORD',
+	'DATABASE_NAME'
+];
+requiredEnvVars.forEach((varName) => {
+	if (!process.env[varName]) {
+		console.error(`Required environment variable '${varName}' is missing`);
+		// Exit the application
+		process.exit(1);
+	}
+});
+console.log(`Connecting to database at ${process.env.DATABASE_HOST}`);
+
 export const db: Pool = mysql.createPool({
 	host: process.env.DATABASE_HOST!,
 	port: Number(process.env.DATABASE_PORT),
