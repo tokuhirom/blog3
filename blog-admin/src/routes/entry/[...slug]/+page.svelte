2e451624b9df02c35008a78d315ccdb08bf27356
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-12-23 11:39:11 +0900

    updated details

--- blog-admin/src/routes/entry/[...slug]/+page.svelte
+++ blog-admin/src/routes/entry/[...slug]/+page.svelte
@@ -4,11 +4,13 @@
 	export let data: PageData;
 </script>
 
-<div>
-    <h2>{data.entry.title}</h2>
-    <p>{data.entry.body}</p>
-    <div>Created: {data.entry.created_at}</div>
-    {#if data.entry.updated_at !== null}
-        <div>Updated: {data.entry.updated_at}</div>
-    {/if}
+<div class="container mx-auto p-4 max-w-3xl bg-white rounded-lg shadow-lg">
+	<h2 class="text-2xl font-bold text-gray-800">{data.entry.title}</h2>
+	<p class="mt-4 text-gray-700 whitespace-pre-line">{data.entry.body}</p>
+	<div class="mt-6 text-sm text-gray-500">
+		<div>Created: <span class="font-medium">{data.entry.created_at}</span></div>
+		{#if data.entry.updated_at !== null}
+			<div>Updated: <span class="font-medium">{data.entry.updated_at}</span></div>
+		{/if}
+	</div>
 </div>
