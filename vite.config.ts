00921b447bb76229312919460f9315853c4a1c67
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-12-23 16:58:38 +0900

    fix: update Vite configuration by removing test settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
-import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
 });
