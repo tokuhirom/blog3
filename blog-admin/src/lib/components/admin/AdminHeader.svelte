3d317637883ccb9cbc41acca76ea0d5410499c6d
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-12-23 11:49:05 +0900

    style: update AdminHeader background color and change link text to 'Blog Admin'

--- blog-admin/src/lib/components/admin/AdminHeader.svelte
+++ blog-admin/src/lib/components/admin/AdminHeader.svelte
@@ -1,10 +1,10 @@
 <script>
 </script>
 
-<header class="fixed left-0 top-0 z-10 w-full bg-gray-800 p-4 text-white">
+<header class="fixed left-0 top-0 z-10 w-full bg-orange-800 p-4 text-white">
 	<div class="container mx-auto flex items-center justify-between">
 		<!-- トップページへのリンク -->
-		<a href="/admin/" class="text-xl font-bold hover:underline">Blog</a>
+		<a href="/admin/" class="text-xl font-bold hover:underline">Blog Admin</a>
 
 		<!-- ナビゲーションリンク -->
 		<nav class="flex space-x-4">
