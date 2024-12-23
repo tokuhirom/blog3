389e9a27d05fcf851814abf3a077b88e886056cb
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-12-23 13:56:06 +0900

    feat: add RSS feed link to layout for blog entries

--- blog-admin/src/routes/+layout.svelte
+++ blog-admin/src/routes/+layout.svelte
@@ -5,6 +5,11 @@
 	let { children } = $props();
 </script>
 
+
+<svelte:head>
+    <link rel="alternate" type="application/rss+xml" title="RSS Feed" href="https://blog.64p.org/feed" />
+</svelte:head>
+
 <div>
 	<PublicHeader />
 	<div class="pt-16">
