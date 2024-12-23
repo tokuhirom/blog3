65d54d5a4555464834fe319f76c5013736b5a65f
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-12-23 12:12:27 +0900

    style: enhance text readability by applying prose class to entry body

--- blog-admin/src/routes/entry/[...slug]/+page.svelte
+++ blog-admin/src/routes/entry/[...slug]/+page.svelte
@@ -7,7 +7,9 @@
 
 <div class="container mx-auto p-4 max-w-3xl bg-white rounded-lg shadow-lg">
 	<h2 class="text-2xl font-bold text-gray-800">{data.entry.title}</h2>
-	<p class="mt-4 text-gray-700 whitespace-pre-line">{@html data.body}</p>
+	<p class="mt-4 text-gray-700 whitespace-pre-line prose">
+        {@html data.body}
+    </p>
 	<div class="mt-6 text-sm text-gray-500">
 		<div>Created: <span class="font-medium">{data.entry.created_at}</span></div>
 		{#if data.entry.updated_at !== null}
