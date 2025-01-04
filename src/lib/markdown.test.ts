9099a31d58ea04f6b2753c7797a802cf77cb2cf1
Tokuhiro Matsuno <tokuhirom@gmail.com>
2025-01-04 09:54:03 +0900

    await

--- src/lib/markdown.test.ts
+++ src/lib/markdown.test.ts
@@ -2,10 +2,10 @@ import { describe, it, expect } from 'vitest';
 import { renderHTML } from './server/markdown';
 
 describe('renderHTML', () => {
-	it('should ignore HTML comments', () => {
+	it('should ignore HTML comments', async () => {
 		const markdown = 'Content\n<!-- This is a comment -->\nMore content';
 		const expectedHtml = '<p>Content</p>\n<p>More content</p>\n';
-		const result = renderHTML(markdown, {});
+		const result = await renderHTML(markdown, {});
 		console.log(result);
 		expect(result).toBe(expectedHtml);
 	});
